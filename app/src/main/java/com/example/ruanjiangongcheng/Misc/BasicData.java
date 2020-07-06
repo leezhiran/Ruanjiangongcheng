b68dfba17e12785888ff8913704e081d8d8d3c47
leezhiran <leezhiran@yeah.net>
2020-07-06 15:36:05 +0800

    72

--- app/src/main/java/com/example/ruanjiangongcheng/Misc/BasicData.java
+++ app/src/main/java/com/example/ruanjiangongcheng/Misc/BasicData.java
@@ -2,12 +2,23 @@ package com.example.ruanjiangongcheng.Misc;
 
 import android.app.Application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BasicData extends Application {
     private String User_id;
     private String Match_id;
-
+    private List<String> Current_history=new ArrayList<>();
     public void setDealt(boolean dealt) {
         this.dealt = dealt;
+    }
+
+    public List<String> getCurrent_history() {
+        return Current_history;
+    }
+
+    public void setCurrent_history(List<String> current_history) {
+        Current_history = current_history;
     }
 
     public boolean isDealt() {
